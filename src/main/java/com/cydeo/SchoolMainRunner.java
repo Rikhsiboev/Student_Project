235891c99e278496b3781b584a29942c48e34278
Kemail-terminal <sulkemref@gmail.com>
2023-02-17 22:14:08 -0600

    Initial commit

--- src/main/java/com/cydeo/SchoolMainRunner.java
+++ src/main/java/com/cydeo/SchoolMainRunner.java
@@ -1,2 +1,16 @@
-package com.cydeo;public class SchoolMainRunner {
+package com.cydeo;
+
+import static com.cydeo.enums.DaysOfWeek.*;
+
+public class SchoolMainRunner {
+
+
+
+    public static void main(String[] args) {
+
+        String Monday = "Monday";
+
+        System.out.println(MONDAY.toString());
+
+    }
 }
--- src/main/java/com/cydeo/enums/DaysOfWeek.java
+++ src/main/java/com/cydeo/enums/DaysOfWeek.java
@@ -1,2 +1,12 @@
-package com.cydeo.enums;public enum DaysOfWeek {
+package com.cydeo.enums;
+
+public enum DaysOfWeek {
+
+    MONDAY("Monday"), TUESDAY("Tuesday");
+
+    String str;
+
+    DaysOfWeek(String str) {
+        this.str = str;
+    }
 }

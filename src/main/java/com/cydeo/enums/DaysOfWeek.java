f508b121eae55f42367bee33c6bad3fa827cf632
Kemail-terminal <sulkemref@gmail.com>
2023-02-19 15:23:24 -0600

    Enum corrected

--- src/main/java/com/cydeo/enums/DaysOfWeek.java
+++ src/main/java/com/cydeo/enums/DaysOfWeek.java
@@ -5,7 +5,7 @@ public enum DaysOfWeek {
     MONDAY("Monday"), TUESDAY("Tuesday"), WEDNESDAY("Wednesday"),
     THURSDAY("Thursday") , FRIDAY("Friday");
 
-    String value;
+    public final String value;
 
     DaysOfWeek(String str) {
         this.value = str;

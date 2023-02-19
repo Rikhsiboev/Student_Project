5afff78495efe18bf963b09e09bae48a96069a3b
Mustafa Cihat YAPICI <cyapicii@gmail.com>
2023-02-19 08:54:33 +0100

    TeacherService.java has been created for creation of business logic. We need to implement all methods.

--- src/main/java/com/cydeo/service/CRUDService.java
+++ src/main/java/com/cydeo/service/CRUDService.java
@@ -1,2 +1,17 @@
-package com.cydeo.service;public class CRUDService {
+package com.cydeo.service;
+
+import java.util.List;
+
+public interface CRUDService<T>{
+
+    T findById(int id);
+
+    List<T> findAll();
+
+    void save(T t);
+
+    void update(T t);
+
+    void deleteById(int id);
+
 }
---
+++ src/main/java/com/cydeo/service/TeacherService.java
@@ -0,0 +1,32 @@
+package com.cydeo.service;
+
+import com.cydeo.entity.Teacher;
+
+import java.util.List;
+
+public class TeacherService implements CRUDService<Teacher>{
+    @Override
+    public Teacher findById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Teacher> findAll() {
+        return null;
+    }
+
+    @Override
+    public void save(Teacher teacher) {
+
+    }
+
+    @Override
+    public void update(Teacher teacher) {
+
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+    }
+}

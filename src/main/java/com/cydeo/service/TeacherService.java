b763021bcc2a47d53f5ab4d1f374cef2fa137905
Mustafa Cihat YAPICI <cyapicii@gmail.com>
2023-02-19 08:58:02 +0100

    TeacherService.java has been created for creation of business logic. We need to implement all methods. Pay attention of TODO.

--- src/main/java/com/cydeo/service/TeacherService.java
+++ src/main/java/com/cydeo/service/TeacherService.java
@@ -4,29 +4,32 @@ import com.cydeo.entity.Teacher;
 
 import java.util.List;
 
-public class TeacherService implements CRUDService<Teacher>{
+public class TeacherService implements CRUDService<Teacher> {
     @Override
     public Teacher findById(int id) {
+        // TODO: 2/19/2023 use stream if possible
         return null;
     }
 
     @Override
     public List<Teacher> findAll() {
+        // TODO: 2/19/2023 use stream if possible
         return null;
     }
 
     @Override
     public void save(Teacher teacher) {
+        // TODO: 2/19/2023 use stream if possible
 
     }
 
     @Override
     public void update(Teacher teacher) {
-
+        // TODO: 2/19/2023 use stream if possible
     }
 
     @Override
     public void deleteById(int id) {
-
+        // TODO: 2/19/2023 use stream if possible
     }
 }

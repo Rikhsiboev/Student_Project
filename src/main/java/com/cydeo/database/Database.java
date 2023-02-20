4751012fb3be9ccdccf773a789e6a601e3f0c969
Rikhsiboev <rikhsiboev@gmail.com>
2023-02-20 10:43:39 -0700

    entity and object now service time

--- src/main/java/com/cydeo/database/Database.java
+++ src/main/java/com/cydeo/database/Database.java
@@ -1,4 +1,18 @@
 package com.cydeo.database;
 
+import com.cydeo.entity.Course;
+import com.cydeo.enums.DaysOfWeek;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Database {
+
+    public static List<Course> courseList = new ArrayList<>(List.of(
+            new Course(1,"English", 40, List.of(DaysOfWeek.MONDAY, DaysOfWeek.THURSDAY)),
+            new Course(2,"Math",50, List.of(DaysOfWeek.values())),
+            new Course(3,"Java",80, List.of(DaysOfWeek.WEDNESDAY))
+
+    ));
+
 }
--- src/main/java/com/cydeo/entity/Course.java
+++ src/main/java/com/cydeo/entity/Course.java
@@ -1,4 +1,15 @@
 package com.cydeo.entity;
 
-public class Course {
+import com.cydeo.enums.DaysOfWeek;
+
+import java.util.List;
+
+public class Course extends BaseEntity {
+    private int id;
+    private String name;
+    private int price;
+    private List<DaysOfWeek> weekDays;
+
+    public Course(int id, String name, int price, List<DaysOfWeek> weekDays) {
+    }
 }

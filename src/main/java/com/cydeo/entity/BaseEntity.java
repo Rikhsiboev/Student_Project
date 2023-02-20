d335e2bc73bb76d6800e325ca8cee32753800c83
Hasanain_Cydeo <hasanainalameen@gmail.com>
2023-02-20 00:13:44 -0600

    Created private int courseId in the class BaseEntity.java

--- src/main/java/com/cydeo/entity/BaseEntity.java
+++ src/main/java/com/cydeo/entity/BaseEntity.java
@@ -1,5 +1,19 @@
 package com.cydeo.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
 public abstract class BaseEntity {
     private int id;
+    private int courseId;
+
+    public BaseEntity(int courseId) {
+        this.courseId = courseId;
+    }
 }

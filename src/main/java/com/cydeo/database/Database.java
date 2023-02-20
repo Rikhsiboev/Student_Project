8cf9c58cf8c172d1c6b59cd70c0c499c4d72bc89
Hasanain_Cydeo <hasanainalameen@gmail.com>
2023-02-20 00:19:33 -0600

    Added List<Course> COURSE_LIST to the Database class

--- src/main/java/com/cydeo/database/Database.java
+++ src/main/java/com/cydeo/database/Database.java
@@ -1,4 +1,11 @@
 package com.cydeo.database;
 
+import com.cydeo.entity.Course;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Database {
+
+    public static final List<Course> COURSE_LIST = new ArrayList<>();
 }

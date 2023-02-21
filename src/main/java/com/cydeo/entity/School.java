b12eaaa5b510238ef79394ea379144dd9cde3bf9
Kemail-terminal <sulkemref@gmail.com>
2023-02-21 14:50:15 -0600

    School class completed

--- src/main/java/com/cydeo/entity/School.java
+++ src/main/java/com/cydeo/entity/School.java
@@ -1,4 +1,24 @@
 package com.cydeo.entity;
 
-public class School {
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class School extends BaseEntity{
+
+    private String name;
+
+    private List<Student> students;
+
+    private List<Course> courses;
+
+    public School(int id, String name, List<Student> students, List<Course> courses) {
+        super(id);
+        this.name = name;
+        this.students = students;
+        this.courses = courses;
+    }
 }

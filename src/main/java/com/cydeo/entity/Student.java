8e4b5802a22da914fea10d46bb7ab5891805738a
ASG <gulisikaynur@gmail.com>
2023-02-20 20:56:16 -0600

    Student entities added.

--- src/main/java/com/cydeo/entity/Student.java
+++ src/main/java/com/cydeo/entity/Student.java
@@ -1,4 +1,28 @@
 package com.cydeo.entity;
 
-public class Student {
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.List;
+
+    @Data
+    @NoArgsConstructor
+    public class Student extends BaseEntity {
+
+
+        private Long studentNumber;
+        private String fistName;
+        private String lastName;
+        private Parent parent;
+        private School school;
+        private List<Course> course;
+
+        public Student(int id, Long studentNumber, String fistName, String lastName, Parent parent, School school, List<Course> course) {
+            super(id);
+            this.studentNumber = studentNumber;
+            this.fistName = fistName;
+            this.lastName = lastName;
+            this.parent = parent;
+            this.school = school;
+            this.course = course;
+        }
 }

219d0ff76e60981fdae62ec02c503043ea2872f6
Kemail-terminal <sulkemref@gmail.com>
2023-02-21 15:13:35 -0600

    School lombok update

--- src/main/java/com/cydeo/entity/School.java
+++ src/main/java/com/cydeo/entity/School.java
@@ -1,12 +1,10 @@
 package com.cydeo.entity;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
 import java.util.List;
 
-@Getter
-@Setter
+@Data
 public class School extends BaseEntity{
 
     private String name;

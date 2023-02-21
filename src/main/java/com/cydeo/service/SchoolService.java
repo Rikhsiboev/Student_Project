0ed3d4f80b03f0c55478a3e1a0ede4fb92bb14fc
Kemail-terminal <sulkemref@gmail.com>
2023-02-21 15:11:31 -0600

    SchoolService class completed

---
+++ src/main/java/com/cydeo/service/SchoolService.java
@@ -0,0 +1,40 @@
+package com.cydeo.service;
+
+import com.cydeo.entity.School;
+
+import static com.cydeo.database.Database.*;
+
+import java.util.List;
+
+public class SchoolService implements CRUDService<School>{
+
+
+    public School findById(int id){
+        return findAll().stream()
+                .filter(p -> p.getId()==id)
+                .findAny().get();
+    }
+
+    @Override
+    public List<School> findAll() {
+        return schoolList;
+    }
+
+    @Override
+    public void save(School school) {
+        findAll().add(school);
+    }
+
+
+    @Override
+    public void update(School school) {
+        findAll().set(
+                findAll().indexOf(findById(school.getId())), school);
+    }
+
+    @Override
+    public void deleteById(int id){
+        findAll().remove(findById(id));
+    }
+
+}

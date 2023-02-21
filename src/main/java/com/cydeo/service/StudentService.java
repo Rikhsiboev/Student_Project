6c0e150b47645c3237a4ca1cb5b18d6323f86c47
denispupkov <denis.pupkov@gmail.com>
2023-02-20 18:58:35 -0700

    deleteById method implemented.

--- src/main/java/com/cydeo/service/StudentService.java
+++ src/main/java/com/cydeo/service/StudentService.java
@@ -27,16 +27,20 @@ public class StudentService implements CRUDService <Student> {
 
     @Override
     public void update(Student student) {
-        //Database.studentList.stream().filter(student1 -> student1.getId()==student.getId()   }
 
         Student s = Database.studentList.stream().filter(student1 ->
                 student1.getId()==student.getId()).findFirst().get();
-       Database.studentList.remove(s);
-       Database.studentList.add(student);
+        Database.studentList.remove(s);
+        Database.studentList.add(student);
     }
 
     @Override
     public void deleteById(int id) {
+        Student s = Database.studentList.stream().filter(student -> student.getId()==id).
+                findFirst().orElseThrow(() ->
+                        new NoSuchElementException("No Student With Matching ID --> " + id));
+        Database.studentList.remove(s);
+
 
     }
 }
